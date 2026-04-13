e7f9fafdf6b2ac66498467ff4580980cb6c3078f
Givaldo Silva <pcejams2021@gmail.com>
2026-04-13 09:49:51 -0300

    Add project display functionality in script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const projetos = [
+    {
+        nome: "Calculadora",
+        descricao: "Projeto simples feito com JavaScript"
+    },
+    {
+        nome: "Site Escolar",
+        descricao: "Desenvolvido com HTML e CSS"
+    },
+    {
+        nome: "Lista de Tarefas",
+        descricao: "Aplicação interativa com JS"
+    }
+];
+
+const container = document.getElementById("lista-projetos");
+
+projetos.forEach(projeto => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
+        <h3>${projeto.nome}</h3>
+        <p>${projeto.descricao}</p>
+    `;
+
+    container.appendChild(card);
+});

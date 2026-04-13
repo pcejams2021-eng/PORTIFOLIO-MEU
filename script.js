8ac21a0abf346522bd77dd86db9741e18aa46f1f
Givaldo Silva <pcejams2021@gmail.com>
2026-04-13 09:52:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const projetos = [
     {
         nome: "Calculadora",
-        descricao: "Projeto simples feito com JavaScript"
+        descricao: "Projeto simples feito com JavaScript",
+        link: "#"
     },
     {
         nome: "Site Escolar",
-        descricao: "Desenvolvido com HTML e CSS"
+        descricao: "Desenvolvido com HTML e CSS",
+        link: "#"
     },
     {
         nome: "Lista de Tarefas",
-        descricao: "Aplicação interativa com JS"
+        descricao: "Aplicação interativa com JS",
+        link: "#"
     }
 ];
 
 const container = document.getElementById("lista-projetos");
+
+// Limpa antes (boa prática)
+container.innerHTML = "";
 
 projetos.forEach(projeto => {
     const card = document.createElement("div");
@@ -22,6 +28,7 @@ projetos.forEach(projeto => {
     card.innerHTML = `
         <h3>${projeto.nome}</h3>
         <p>${projeto.descricao}</p>
+        <a href="${projeto.link}" target="_blank">Ver projeto</a>
     `;
 
     container.appendChild(card);
